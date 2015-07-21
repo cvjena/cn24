9e079abfd8eeab020db4d1ee0d74dbd219927f43
Clemens-Alexander Brust <ikosa.de@gmail.com>
2015-07-21 12:18:46 +0200

    GradientAccumulationLayer: Fixed header

--- include/cn24/net/GradientAccumulationLayer.h
+++ include/cn24/net/GradientAccumulationLayer.h
@@ -1,3 +1,9 @@
+/*
+ * This file is part of the CN24 semantic segmentation software,
+ * copyright (C) 2015 Clemens-Alexander Brust (ikosa dot de at gmail dot com).
+ *
+ * For licensing information, see the LICENSE file included with this project.
+ */  
 /**
  * @file GradientAccumulationLayer.h
  * @class GradientAccumulationLayer
