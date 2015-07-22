0fb0c45278c63d7004b55847571755c93c2775c9
Clemens-Alexander Brust <ikosa.de@gmail.com>
2015-07-22 12:23:56 +0200

    LocalResponseNormalizationLayer: Completed forward pass

--- src/net/LocalResponseNormalizationLayer.cpp
+++ src/net/LocalResponseNormalizationLayer.cpp
@@ -5,6 +5,7 @@
  * For licensing information, see the LICENSE file included with this project.
  */  
 #include <limits>
+#include <cmath>
 
 #include "Log.h"
 #include "LocalResponseNormalizationLayer.h"
@@ -68,7 +69,50 @@ bool LocalResponseNormalizationLayer::
 }
 
 void LocalResponseNormalizationLayer::FeedForward() {
-  
+  const int sub = (size_-1)/2;
+  const int add = (size_)/2;
+  #pragma omp parallel for default(shared)
+  for(unsigned int sample = 0; sample < input_->data.samples(); sample++) {
+    if(normalization_method_ == WITHIN_CHANNELS) {
+      for(unsigned int map = 0; map < maps_; map++) {
+        for(unsigned int y = 0; y < input_height_; y++) {
+          for(unsigned int x = 0; x < input_height_; x++) {
+            datum region_sum = 0;
+            unsigned int region_size = 0;
+            for(unsigned int iy = (((int)y-sub) > 0 ? (int)y-sub : 0); (iy < input_height_) && (iy <= ((int)y+add)); iy++) {
+              for(unsigned int ix = (((int)x-sub) > 0 ? (int)x-sub : 0); (ix < input_width_) && (ix <= ((int)x+add)); ix++) {
+                const datum input_value = (*input_->data.data_ptr_const(ix,iy,map,sample));
+                region_sum += input_value * input_value;
+                region_size++;
+              }
+            }
+            
+            datum divisor = pow(1.0 + ((alpha_/((datum)region_size))*region_sum), beta_);
+            (*output_->data.data_ptr(x,y,map,sample)) = (*input_->data.data_ptr_const(x,y,map,sample)) / divisor;
+          }
+        }
+      }
+    } else if(normalization_method_ == ACROSS_CHANNELS) {
+      for(unsigned int map = 0; map < maps_; map++) {
+        for(unsigned int y = 0; y < input_height_; y++) {
+          for(unsigned int x = 0; x < input_height_; x++) {
+            datum region_sum = 0;
+            unsigned int region_size = 0;
+            for(unsigned int imap = (((int)map-sub) > 0 ? (int)map-sub : 0); (imap < maps_) && (imap <= ((int)map+add)); imap++) {
+              const datum input_value = (*input_->data.data_ptr_const(x,y,imap,sample));
+              region_sum += input_value * input_value;
+              region_size++;
+            }
+            
+            datum divisor = pow(1.0 + ((alpha_/((datum)region_size))*region_sum), beta_);
+            (*output_->data.data_ptr(x,y,map,sample)) = (*input_->data.data_ptr_const(x,y,map,sample)) / divisor;
+          }
+        }
+      }
+    } else {
+      FATAL("Unknown normalization method");
+    }
+  }
 }
 
 void LocalResponseNormalizationLayer::BackPropagate() {
