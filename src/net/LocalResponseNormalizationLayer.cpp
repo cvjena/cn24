2a7564a97f297c4acdec67539ee2d8a71530139d
Clemens-Alexander Brust <ikosa.de@gmail.com>
2015-07-25 12:38:39 +0200

    LocalResponseNormalizationLayer: Fixed typo

--- src/net/LocalResponseNormalizationLayer.cpp
+++ src/net/LocalResponseNormalizationLayer.cpp
@@ -18,7 +18,7 @@ LocalResponseNormalizationLayer::
     const LocalResponseNormalizationLayer::NormalizationMethod normalization_method) :
     size_(size), alpha_(alpha), beta_(beta), normalization_method_(normalization_method) {
   LOGDEBUG << "Instance created, size: " << size_ << ", alpha: " << alpha_ 
-  << ", beta: " << beta_ << ", method: " << (normalization_method_ == ACROSS_CHANNELS) ? "across" : "within";
+  << ", beta: " << beta_ << ", method: " << ((normalization_method_ == ACROSS_CHANNELS) ? "across" : "within");
 
 }
 
