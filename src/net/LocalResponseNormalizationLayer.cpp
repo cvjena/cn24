c14dbb629f356ee786e99de75e663d7571a8cc46
Clemens-Alexander Brust <ikosa.de@gmail.com>
2015-08-18 20:52:02 +0200

    LocalResponseNormalizationLayer: Added backwards pass, layer implementation now complete

--- src/net/LocalResponseNormalizationLayer.cpp
+++ src/net/LocalResponseNormalizationLayer.cpp
@@ -119,7 +119,72 @@ void LocalResponseNormalizationLayer::FeedForward() {
 void LocalResponseNormalizationLayer::BackPropagate() {
 //  FATAL("Backward pass missing, use only for prediction!");
   TensorMath::SETSAMPLE(input_->delta, -1, 0);
+  
+  
+  const int sub = (size_-1)/2;
+  const int add = (size_)/2;
+  #pragma omp parallel for default(shared)
+  for(unsigned int sample = 0; sample < input_->data.samples(); sample++) {
+    if(normalization_method_ == WITHIN_CHANNELS) {
+      for(unsigned int map = 0; map < maps_; map++) {
+        for(unsigned int y = 0; y < input_height_; y++) {
+          for(unsigned int x = 0; x < input_height_; x++) {
+            datum region_sum = 0;
+            unsigned int region_size = 0;
+            for(unsigned int iy = (((int)y-sub) > 0 ? (int)y-sub : 0); (iy < input_height_) && (iy <= ((int)y+add)); iy++) {
+              for(unsigned int ix = (((int)x-sub) > 0 ? (int)x-sub : 0); (ix < input_width_) && (ix <= ((int)x+add)); ix++) {
+                const datum input_value = (*input_->data.data_ptr_const(ix,iy,map,sample));
+                region_sum += input_value * input_value;
+                region_size++;
+              }
+            }
+            
+            datum divisor = pow(1.0 + ((alpha_/((datum)region_size))*region_sum), beta_);
+            datum divisor2 = divisor * divisor;
+            const datum xi = *input_->data.data_ptr_const(x,y,map,sample);
+            const datum dxi = *output_->delta.data_ptr_const(x,y,map,sample);
+            
+            for(unsigned int iy = (((int)y-sub) > 0 ? (int)y-sub : 0); (iy < input_height_) && (iy <= ((int)y+add)); iy++) {
+              for(unsigned int ix = (((int)x-sub) > 0 ? (int)x-sub : 0); (ix < input_width_) && (ix <= ((int)x+add)); ix++) {
+                const datum xj = *input_->data.data_ptr_const(ix,iy,map,sample);
+                (*input_->delta.data_ptr(ix,iy,map,sample)) -= (dxi * xi * (datum)2.0 * beta_ * (alpha_ / (datum)region_size) * xj *
+                  (datum)pow((datum)1.0 + (alpha_ / (datum)region_size) * region_sum, beta_ - (datum)1.0)) / divisor2;
+              }
+            }
+            (*input_->delta.data_ptr(x,y,map,sample)) += dxi / divisor;
+          }
+        }
+      }
+    } else if(normalization_method_ == ACROSS_CHANNELS) {
+      for(unsigned int map = 0; map < maps_; map++) {
+        for(unsigned int y = 0; y < input_height_; y++) {
+          for(unsigned int x = 0; x < input_height_; x++) {
+            datum region_sum = 0;
+            unsigned int region_size = 0;
+            for(unsigned int imap = (((int)map-sub) > 0 ? (int)map-sub : 0); (imap < maps_) && (imap <= ((int)map+add)); imap++) {
+              const datum input_value = (*input_->data.data_ptr_const(x,y,imap,sample));
+              region_sum += input_value * input_value;
+              region_size++;
+            }
+            
+            datum divisor = pow(1.0 + ((alpha_/((datum)region_size))*region_sum), beta_);
+            datum divisor2 = divisor * divisor;
+            const datum xi = *input_->data.data_ptr_const(x,y,map,sample);
+            const datum dxi = *output_->delta.data_ptr_const(x,y,map,sample);
+            
+            for(unsigned int imap = (((int)map-sub) > 0 ? (int)map-sub : 0); (imap < maps_) && (imap <= ((int)map+add)); imap++) {
+                const datum xj = *input_->data.data_ptr_const(x,y,imap,sample);
+                (*input_->delta.data_ptr(x,y,imap,sample)) -= (dxi * xi * (datum)2.0 * beta_ * (alpha_ / (datum)region_size) * xj *
+                  (datum)pow((datum)1.0 + (alpha_ / (datum)region_size) * region_sum, beta_ - (datum)1.0)) / divisor2;
+            }
+            (*input_->delta.data_ptr(x,y,map,sample)) += dxi / divisor;
+          }
+        }
+      }
+    } else {
+      FATAL("Unknown normalization method");
+    }
+  }
 }
-
 
 }
