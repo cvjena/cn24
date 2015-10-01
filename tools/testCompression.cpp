bc9820f3f3015d528b1f78547930821264bb3b28
Clemens-Alexander Brust <ikosa.de@gmail.com>
2015-10-01 11:16:09 +0200

    testCompression: Now serializes a compressed TensorStream

--- tools/testCompression.cpp
+++ tools/testCompression.cpp
@@ -10,42 +10,24 @@
 #include <iostream>
 #include <fstream>
 
-struct CompressedTensor {
-public:
-  unsigned int compressed_length;
-  void* compressed_data;
-};
-
-const unsigned char rl_marker = 'X';
-const unsigned char rl_doublemarker = 'X';
-const unsigned char rl_rle = 'Y';
-const unsigned int rl_bytes = 1;
-const unsigned int chars_per_datum = sizeof(Conv::datum)/sizeof(char);
-const unsigned int rl_max = (unsigned int)((1L << (8L * (unsigned long)rl_bytes)) - 3L);
-const unsigned int rl_min = 1 + (5 + rl_bytes) / chars_per_datum;
-
-void Compress(const Conv::Tensor& tensor, CompressedTensor* compressed);
-unsigned int Decompress(Conv::Tensor& tensor, CompressedTensor* compressed);
-
 int main(int argc, char** argv) {
   Conv::System::Init();
   
-  if(argc != 2) {
-    FATAL("Needs exactly two arguments!");
+  if(argc != 3) {
+    FATAL("Needs exactly two arguments");
   }
   
-  LOGINFO << "rl_bytes: " << rl_bytes;
-  LOGINFO << "rl_max  : " << rl_max;
-  LOGINFO << "rl_min  : " << rl_min;
-  
-  CompressedTensor compressed;
   std::string input_file_name(argv[1]);
-  //std::string output_file_name(argv[2]);
+  std::string output_file_name(argv[2]);
   
-  std::ifstream input_tensor_stream(input_file_name);
-  //std::ofstream output_tensor_stream(output_file_name);
+  std::ifstream input_tensor_stream(input_file_name, std::ios::in | std::ios::binary);
+  std::ofstream output_tensor_stream(output_file_name, std::ios::out | std::ios::binary);
+  
   if(!input_tensor_stream.good())
     FATAL("Cannot open " << input_file_name);
+  
+  if(!output_tensor_stream.good())
+    FATAL("Cannot open " << output_file_name);
   
   long uncompressed_total = 0;
   long compressed_total = 0;
@@ -56,22 +38,25 @@ int main(int argc, char** argv) {
     
     LOGDEBUG << "Input tensor: " << tensor;
     
-    LOGDEBUG << "Size: " << tensor.elements() * sizeof(Conv::datum)/sizeof(char);
+    unsigned int original_size = tensor.elements() * sizeof(Conv::datum)/sizeof(char);
+    LOGDEBUG << "Size: " << original_size;
     
-    Compress(tensor, &compressed);
-    LOGDEBUG << "RLE Size: " << compressed.compressed_length;
-    unsigned int bytes_out = Decompress(tensor, &compressed);
+    Conv::CompressedTensor ctensor;
+    ctensor.Compress(tensor);
     
-    //tensor.Serialize(output_tensor_stream,false);
+    ctensor.Serialize(output_tensor_stream);
     
-    free(compressed.compressed_data);
+    LOGDEBUG << "RLE Size: " << ctensor.compressed_length();
     
-    if(bytes_out != (tensor.elements() * sizeof(Conv::datum)/sizeof(char))) {
+    ctensor.Decompress(tensor);
+    unsigned int bytes_out = tensor.elements() * sizeof(Conv::datum)/sizeof(char);
+    
+    if(bytes_out != original_size) {
       FATAL("Size mismatch! Expected: " << (tensor.elements() * sizeof(Conv::datum)/sizeof(char)) << ", actual: " << bytes_out);
     }
     
-    LOGINFO << "Ratio: " << 100.0 * (double)compressed.compressed_length / (double)(tensor.elements() * sizeof(Conv::datum)/sizeof(char)) << "%" << std::flush;
-    compressed_total += compressed.compressed_length;
+    LOGINFO << "Ratio: " << 100.0 * (double)ctensor.compressed_length() / (double)(tensor.elements() * sizeof(Conv::datum)/sizeof(char)) << "%" << std::flush;
+    compressed_total += ctensor.compressed_length();
     uncompressed_total += tensor.elements() * sizeof(Conv::datum)/sizeof(char);
     
     input_tensor_stream.peek();
@@ -80,136 +65,4 @@ int main(int argc, char** argv) {
   LOGINFO << "Uncompressed: " << uncompressed_total;
   LOGINFO << "Compressed  : " << compressed_total;
   LOGEND;
-}
-
-
-
-
-void Compress(const Conv::Tensor& tensor, CompressedTensor* compressed) {
-  
-  unsigned int bytes_out = 0;
-  
-  Conv::datum last_symbol = 0;
-  unsigned int running_length = 0;
-  
-  unsigned char* output_ptr = new unsigned char[2 * tensor.elements() * chars_per_datum];
-  compressed->compressed_data = (void*)output_ptr;
-  for(unsigned int pos = 0; pos <= tensor.elements(); pos++) {
-    Conv::datum current_symbol;
-    if(pos < tensor.elements()) {
-      current_symbol = tensor.data_ptr_const()[pos];
-      if(current_symbol == last_symbol) {
-        // Increase running length
-        running_length++;
-      }
-    } else {
-      // Force emission of last symbol
-    }
-    
-    
-    if(
-    // EOF reached
-    (pos == (tensor.elements())) ||
-    // Different symbol
-    (current_symbol != last_symbol) ||
-    // Maxmimum run length reached
-    (running_length == rl_max)) {
-        
-      // Emit...
-      if(running_length > 0 && running_length < rl_min) {
-        // Emit single symbol(s)
-        for(unsigned int r = 0; r < running_length; r++) {
-          for(unsigned int b = 0; b < chars_per_datum; b++) {
-            char char_to_emit = ((char*)&last_symbol)[b];
-            if(char_to_emit == rl_marker) {
-              // Emit escaped
-              *output_ptr = rl_marker;
-              output_ptr++; bytes_out++;
-              *output_ptr = rl_doublemarker;
-              output_ptr++; bytes_out++;
-            } else {
-              // Emit directly
-              *output_ptr = char_to_emit;
-              output_ptr++; bytes_out++;
-            }
-          }
-        }
-      } else if(running_length >= rl_min) {
-        // Emit encoded
-        *output_ptr = rl_marker;
-        output_ptr++; bytes_out++;
-        *output_ptr = rl_rle;
-        output_ptr++; bytes_out++;
-        
-        // Running length output
-        for(unsigned int b = 0; b < rl_bytes; b++) {
-          *output_ptr = (running_length >> ((rl_bytes - (b+1)) * 8)) & 0xFF;
-          output_ptr++; bytes_out++;
-        }
-        
-        for(unsigned int b = 0; b < chars_per_datum; b++) {
-          unsigned char char_to_emit = ((char*)&last_symbol)[b];
-          *output_ptr = char_to_emit;
-          output_ptr++; bytes_out++;
-        }
-      }
-        
-      // ...and reset
-      if(running_length == rl_max)
-        running_length = 0;
-      else
-        running_length = 1;
-    }
-      
-    last_symbol = current_symbol;
-  }
-  LOGDEBUG << "Bytes out: " << bytes_out;
-  compressed->compressed_length = bytes_out;
-  
-}
-
-unsigned int Decompress(Conv::Tensor& tensor, CompressedTensor* compressed) {
-  unsigned int bytes_out = 0;
-  unsigned char* output_ptr = (unsigned char*)tensor.data_ptr();
-  const unsigned char* input_ptr = (const unsigned char*)compressed->compressed_data;
-  
-  for(unsigned int pos = 0; pos < compressed->compressed_length; pos++) {
-    unsigned char current_symbol = input_ptr[pos];
-    if(current_symbol == rl_marker) {
-      pos++; current_symbol = input_ptr[pos];
-      if(current_symbol == rl_doublemarker) {
-        // Emit single marker
-        *output_ptr = rl_marker;
-        output_ptr++; bytes_out++;
-      } else if(current_symbol == rl_rle) {
-        unsigned int running_length = 0;
-        
-        // Running length input
-        for(unsigned int b = 0; b < rl_bytes; b++) {
-          pos++; current_symbol = input_ptr[pos];
-          running_length += current_symbol;
-          if((b+1) != rl_bytes)
-            running_length <<= 8;
-        }
-        
-        for(unsigned int r = 0; r < running_length; r++) {
-          for(unsigned int b = 0; b < chars_per_datum; b++) {
-            pos++; current_symbol = input_ptr[pos];
-            *output_ptr = current_symbol;
-            output_ptr++; bytes_out++;
-          }
-          pos -= chars_per_datum;
-        }
-        pos += chars_per_datum;
-      } else {
-        FATAL("Incorrect encoding!");
-      }
-    } else {
-      // Emit directly
-      *output_ptr = current_symbol;
-      output_ptr++; bytes_out++;
-    }
-  }
-  LOGDEBUG << "Bytes out: " << bytes_out;
-  return bytes_out;
 }
