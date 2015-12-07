f5f83f91daef9bf9ae12d4262ef207c8248eb347
Clemens-Alexander Brust <ikosa.de@gmail.com>
2015-12-07 16:01:29 +0100

    ConsoleStatSink: Using LOGRESULT and LOGTRESULT now

--- include/cn24/util/ConsoleStatSink.h
+++ include/cn24/util/ConsoleStatSink.h
@@ -37,10 +37,11 @@ public:
     }
   }
   virtual void Process(HardcodedStats& hardcoded_stats, std::vector<Stat*>& stats) {
-    LOGDEBUG << "Stats for epoch " << hardcoded_stats.epoch << ":";
+    (hardcoded_stats.is_training ? LOGTRESULT : LOGRESULT) << "Stats for epoch " << hardcoded_stats.epoch << ":";
     for(unsigned int s = 0; s < stat_descriptors_.size(); s++) {
-      if(!stats[s]->is_null)
-        LOGDEBUG << std::setw(32) << stat_descriptors_[s]->description << ": " << std::setw(24) << stats[s]->value << " " << stat_descriptors_[s]->unit;
+      if(!stats[s]->is_null) {
+        (hardcoded_stats.is_training ? LOGTRESULT : LOGRESULT) << std::setw(32) << stat_descriptors_[s]->description << ": " << std::setw(24) << stats[s]->value << " " << stat_descriptors_[s]->unit;
+      }
     }
   }
   
