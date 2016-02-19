59b4f5a0a9e3a7d41c1c92a3e1b13f3018b37719
Erik Rodner <Erik.Rodner@uni-jena.de>
2016-02-19 22:07:32 +0100

    re-working the derivatives of HMAX

--- src/net/HMaxActivationFunction.cpp
+++ src/net/HMaxActivationFunction.cpp
@@ -82,21 +82,51 @@ void HMaxActivationFunction::BackPropagate() {
   const datum a = weights_->data.data_ptr_const()[0];
   const datum b = weights_->data.data_ptr_const()[1];
   
-  datum delta_a = -((datum)(input_->data.elements()))/a;
-  datum delta_b = -(datum)(input_->data.elements());
+  // weights the regularization term
+  const datum lambda_sparse_regularization = 0.1;
+
+  // TODO: This layer needs to produce a loss (aka regularizer) as well
+  // that is added directly to the objective
+
+  // calculate part of the derivative of the KL criterion
+  // which is constant for all dimensions
+  // https://figss.uni-frankfurt.de/~triesch/publications/Triesch-ICANN2005.pdf
+  datum delta_a = -((datum)(input_->data.elements()))/a * lambda_sparse_regularization;
+  datum delta_b = -(datum)(input_->data.elements()) * lambda_sparse_regularization;
 #pragma omp parallel for default(shared)
   for (std::size_t element = 0; element < input_->data.elements(); element++) {
     const datum input_data = input_->data.data_ptr_const() [element];
     const datum output_data = output_->data.data_ptr_const() [element];
     const datum output_delta = output_->delta.data_ptr_const ()[element];
+    const datum output_mix = (1-output_data) * output_data;
 
     // Calculate derivative
-    const datum input_delta = output_delta *  a * exp(-(a * input_data + b)) * output_data * output_data;
+    // o = (1 + exp(-ax - b))^{-1}
+    // https://en.wikipedia.org/wiki/Logistic_function
+    // do/dx = o (1-o) * a
+    // do/da = o (1-o) * x
+    // do/db = o (1-o)
+    datum input_delta = output_delta * a * output_mix; 
+    
+    // Calculate gradient of the output w.r.t. to weights
+    delta_a += output_mix * input_data;
+    delta_b += output_mix;
+
+    // handle the KL divergence regularization;
+    // We need to add the derivative of eq. (4) w.r.t. x here to input_delta
+    // weighted by lambda_sparse_regularization
+    // d/(dx) o log o = do/dx * log o + o * do/dx * o^-1
+    //                = do/dx ( log o + 1 )
+    input_delta += lambda_sparse_regularization * ( output_mix * (log(output_data) + 1) );
+    input_delta += lambda_sparse_regularization * output_mix / mu_;
+
     input_->delta.data_ptr() [element] = input_delta;
     
     // Calculate gradient of KL-divergence w.r.t. weights
-    delta_a += -input_data + (2.0 + 1.0/mu_) * input_data * output_data - (1.0/mu_) * input_data * output_data * output_data;
-    delta_b += (2.0 + 1.0/mu_) * output_data - (1.0/mu_) * output_data * output_data;
+    delta_a += (-input_data + (2.0 + 1.0/mu_) * input_data * output_data - input_data * output_data * output_data / mu_) 
+        * lambda_sparse_regularization;
+    delta_b += ((2.0 + 1.0/mu_) * output_data - output_data * output_data / mu_) 
+        * lambda_sparse_regularization;
   } 
   
   weights_->delta.data_ptr()[0] = local_lr_ * delta_a;
