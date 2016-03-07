a7f49061aa2bba687ff591f6f3cbad54cfe1e907
Clemens-Alexander Brust <ikosa.de@gmail.com>
2016-03-07 20:08:44 +0100

    tests: Added tests for LayerFactory

---
+++ tests/LayerFactoryConfigurationExtraction.cpp
@@ -0,0 +1,48 @@
+/*
+ * This file is part of the CN24 semantic segmentation software,
+ * copyright (C) 2015 Clemens-Alexander Brust (ikosa dot de at gmail dot com).
+ *
+ * For licensing information, see the LICENSE file included with this project.
+ */
+
+#include <cn24.h>
+
+#include <vector>
+#include <string>
+
+int main(int argc, char* argv[]) {
+  Conv::System::Init();
+  
+  std::vector<std::string> descriptors = {
+    "convolution(size=3x3 pad=3x4)",
+    "convolution(size=3x3)",
+    "convolution()",
+    "convolution"
+  };
+  std::vector<std::string> expected_configurations = {
+    "size=3x3 pad=3x4",
+    "size=3x3",
+    "",
+    ""
+  };
+  
+  bool failed = false;
+  
+  for(unsigned int s = 0; s < descriptors.size(); s++) {
+    std::string& descriptor = descriptors[s];
+    std::string& excpected_configuration = expected_configurations[s];
+    std::string actual_configuration = Conv::LayerFactory::ExtractConfiguration(descriptor);
+    if(actual_configuration.compare(excpected_configuration) != 0) {
+      LOGERROR << "Extracting descriptor " << descriptor << ", expected: " << excpected_configuration
+      << ", actual: " << actual_configuration;
+      failed = 1;
+    }
+  }
+  
+  LOGEND;
+  if(failed) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
---
+++ tests/LayerFactoryValidation.cpp
@@ -0,0 +1,53 @@
+/*
+ * This file is part of the CN24 semantic segmentation software,
+ * copyright (C) 2015 Clemens-Alexander Brust (ikosa dot de at gmail dot com).
+ *
+ * For licensing information, see the LICENSE file included with this project.
+ */
+
+#include <cn24.h>
+
+#include <vector>
+#include <string>
+
+int main(int argc, char* argv[]) {
+  Conv::System::Init();
+  
+  std::vector<std::string> working_descriptors = {
+    "convolution(size=3x3 pad=3x4)",
+    "convolution(size=3x3)",
+    "convolution()",
+    "convolution"
+  };
+  std::vector<std::string> nonworking_descriptors = {
+    "",
+    "convolution(size=3x3 )",
+    "convolution(size=3x3)()",
+    "convolution(size=4x4, pad=2x2)",
+    "?convolution(size=4x4 pad=2x2)",
+    "convolution size=4x4 pad=2x2"
+  };
+  
+  bool failed = false;
+  
+  for(const std::string& descriptor : working_descriptors) {
+    if(!Conv::LayerFactory::IsValidDescriptor(descriptor)) {
+      LOGERROR << "Valid descriptor rejected: " << descriptor;
+      failed = true;
+    }
+  }
+  
+  for(const std::string& descriptor : nonworking_descriptors) {
+    if(Conv::LayerFactory::IsValidDescriptor(descriptor)) {
+      LOGERROR << "Invalid descriptor accepted: " << descriptor;
+      failed = true;
+    }
+  }
+  
+  LOGEND;
+  if(failed) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
