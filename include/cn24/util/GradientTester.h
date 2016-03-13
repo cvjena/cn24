e9b4c26f2c34d19f1f8aee8a716824ba4229fb61
Clemens-Alexander Brust <ikosa.de@gmail.com>
2016-03-13 20:06:11 +0100

    GradientTester: Added more flexible gradient test function

--- include/cn24/util/GradientTester.h
+++ include/cn24/util/GradientTester.h
@@ -25,6 +25,8 @@ public:
    * Only call this function on nets with a constant input, not a DatasetInputLayer!
    */
   static void TestGradient(NetGraph& net, unsigned int skip_weights = 0, bool fatal_fail = false);
+  
+  static bool DoGradientTest(Conv::Layer* layer, Conv::Tensor& data, Conv::Tensor& delta, std::vector<Conv::CombinedTensor*>& outputs, Conv::datum epsilon, void (*WriteLossDeltas)(const std::vector<CombinedTensor*>&), datum (*CalculateLoss)(const std::vector<CombinedTensor*>&));
 };
   
   
--- src/util/GradientTester.cpp
+++ src/util/GradientTester.cpp
@@ -5,6 +5,8 @@
  * For licensing information, see the LICENSE file included with this project.
  */  
 
+#include <iomanip>
+
 #include "Log.h"
 #include "GradientTester.h"
 
@@ -108,8 +110,57 @@ graph.FeedForward();
   if (global_failed > 0 && fatal_fail) {
     FATAL("Failed gradient check!");
   }
+  
 
 }
-
   
+bool GradientTester::DoGradientTest(Conv::Layer* layer, Conv::Tensor& data, Conv::Tensor& delta, std::vector<Conv::CombinedTensor*>& outputs, Conv::datum epsilon, void (*WriteLossDeltas)(const std::vector<CombinedTensor*>&), datum (*CalculateLoss)(const std::vector<CombinedTensor*>&)) {
+  layer->FeedForward();
+  WriteLossDeltas(outputs);
+  layer->BackPropagate();
+  
+  unsigned int elements = data.elements();
+  unsigned int okay = 0;
+
+  // Weight gradient test
+  for (unsigned int w = 0; w < data.elements(); w++) {
+    const Conv::datum weight = data.data_ptr_const()[w];
+    const Conv::datum gradient = delta.data_ptr_const()[w];
+
+    // Using central diff
+    data.data_ptr()[w] = weight + epsilon;
+    layer->FeedForward();
+    const Conv::datum forward_loss = CalculateLoss(outputs);
+
+    data.data_ptr()[w] = weight - epsilon;
+    layer->FeedForward();
+    const Conv::datum backward_loss = CalculateLoss(outputs);
+
+    const Conv::datum fd_gradient = (forward_loss - backward_loss) / (2.0 * epsilon);
+    data.data_ptr()[w] = weight;
+
+    const Conv::datum ratio = fd_gradient / gradient;
+    if(ratio > 1.2 || ratio < 0.8) {
+      LOGDEBUG << "BP Grad : " << gradient;
+      LOGDEBUG << "FD Grad : " << fd_gradient;
+      LOGDEBUG << "Ratio   : " << ratio;
+      LOGDEBUG << "Diff    : " << gradient - fd_gradient;
+    } else {
+      okay++;
+    }
+  }
+  if(okay != elements) {
+    double success_rate = (double)okay/(double)elements;
+    if(success_rate > 0.85)
+      return true;
+    else {
+      LOGERROR << okay << " of " << elements << " gradients okay - " << std::setprecision(3) << 100.0 * (double)okay/(double)elements << "%";
+      return false;
+    }
+  } else {
+    return true;
+  }
+}
+
+
 }
