dfff76d14a404ee41f1eb6986d30a08a6d33373f
Clemens-Alexander Brust <ikosa.de@gmail.com>
2016-06-16 19:47:34 +0200

    [util] ILSVRCDataset: Added more info to synset parsing

--- src/util/ILSVRCDataset.cpp
+++ src/util/ILSVRCDataset.cpp
@@ -5,6 +5,7 @@
  * For licensing information, see the LICENSE file included with this project.
  */
 
+#include <iostream>
 #include <fstream>
 
 #include "ILSVRCDataset.h"
@@ -17,16 +18,24 @@ ILSVRCDataset::ILSVRCDataset() {
 
 void ILSVRCDataset::Load(JSON descriptor) {
   std::string path = descriptor["ilsvrc_path"];
-  std::ifstream synset_file = std::ifstream(path + "/synsets.json", std::ios::in);
+  std::string descriptor_path = path + "/synsets.json";
+  std::ifstream synset_file = std::ifstream(descriptor_path, std::ios::in);
   if(!synset_file.good()) {
     FATAL("Cannot open synset descriptor file, looking at " + path + "/synsets.json");
   }
-  JSON synset_json = JSON::parse(synset_file);
-  if(synset_json.count("synsets") == 0) {
+  JSON synset_file_json = JSON::parse(synset_file);
+  if(synset_file_json.count("synsets") == 0) {
     FATAL("No synsets found in descriptor file!");
   }
-  JSON synsets = synset_json["synsets"];
-  LOGINFO << "Loading " << synsets.size() << " synsets...";
+  JSON synsets_json = synset_file_json["synsets"];
+  LOGINFO << "Loading " << synsets_json.size() << " synsets...";
+
+
+  for(JSON::iterator synset_json_iterator = synsets_json.begin(); synset_json_iterator != synsets_json.end(); ++synset_json_iterator) {
+    std::string WNID = synset_json_iterator.key();
+    JSON synset_json = synset_json_iterator.value();
+    LOGDEBUG << "Processing " << WNID << "...";
+  }
   classes_ = 1;
   testing_samples_ = 0;
   training_samples_ = 0;
