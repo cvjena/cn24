79cbd214f33c4c5eb99ee8d08dbe6a5678de12df
Clemens-Alexander Brust <ikosa.de@gmail.com>
2016-08-27 16:57:24 +0200

    [net] YOLOLossLayer: Started work

---
+++ include/cn24/net/YOLOLossLayer.h
@@ -0,0 +1,60 @@
+/*
+ * This file is part of the CN24 semantic segmentation software,
+ * copyright (C) 2015 Clemens-Alexander Brust (ikosa dot de at gmail dot com).
+ *
+ * For licensing information, see the LICENSE file included with this project.
+ */  
+/**
+ * @file YOLOLossLayer.h
+ * @class YOLOLossLayer
+ * @brief This Layer calculates the loss according to the YOLO paper
+ * 
+ * @author Clemens-Alexander Brust (ikosa dot de at gmail dot com)
+ */
+
+#ifndef CONV_YOLOLOSSLAYER_H
+#define CONV_YOLOLOSSLAYER_H
+
+#include <string>
+#include <sstream>
+
+#include "Layer.h"
+#include "LossFunctionLayer.h"
+
+namespace Conv {
+  
+class YOLOLossLayer : public Layer, public LossFunctionLayer {
+public:
+  /**
+   * @brief Constructs an YOLOLossLayer.
+   */
+  YOLOLossLayer(JSON configuration);
+  
+  // Implementations for Layer
+  bool CreateOutputs (const std::vector< CombinedTensor* >& inputs,
+                      std::vector< CombinedTensor* >& outputs);
+  bool Connect (const std::vector< CombinedTensor* >& inputs,
+                const std::vector< CombinedTensor* >& outputs,
+                const NetStatus* net );
+  void FeedForward();
+  void BackPropagate(); 
+  
+  // Implementations for LossFunctionLayer
+  datum CalculateLossFunction();
+  
+	std::string GetLayerDescription() {
+		std::ostringstream ss;
+		ss << "YOLO Loss Layer (Weight: " << loss_weight_ << ")";
+		return ss.str();
+	}
+private:
+  CombinedTensor* first_ = nullptr;
+  CombinedTensor* second_ = nullptr;
+  CombinedTensor* third_ = nullptr;
+
+	datum loss_weight_ = 1.0;
+};
+
+}
+
+#endif
---
+++ src/net/YOLOLossLayer.cpp
@@ -0,0 +1,98 @@
+/*
+ * This file is part of the CN24 semantic segmentation software,
+ * copyright (C) 2015 Clemens-Alexander Brust (ikosa dot de at gmail dot com).
+ *
+ * For licensing information, see the LICENSE file included with this project.
+ */
+#include <cmath>
+
+#include "Log.h"
+#include "CombinedTensor.h"
+
+#include "YOLOLossLayer.h"
+
+namespace Conv {
+
+YOLOLossLayer::YOLOLossLayer(JSON configuration)
+ : Layer(configuration) {
+  LOGDEBUG << "Instance created.";
+#ifdef ERROR_LAYER_IGNORE_WEIGHTS
+  LOGINFO << "Weights are being ignored!";
+#endif
+}
+
+bool YOLOLossLayer::CreateOutputs ( const std::vector< CombinedTensor* >& inputs,
+                                 std::vector< CombinedTensor* >& outputs ) {
+  UNREFERENCED_PARAMETER(outputs);
+  // Validate input node count
+  if ( inputs.size() != 3 ) {
+    LOGERROR << "Need exactly 3 inputs to calculate loss function!";
+    return false;
+  }
+
+  CombinedTensor* first = inputs[0];
+  CombinedTensor* second = inputs[1];
+  CombinedTensor* third = inputs[2];
+
+  // Check for null pointers
+  if ( first == nullptr || second == nullptr || third == nullptr ) {
+    LOGERROR << "Null pointer node supplied";
+    return false;
+  }
+
+  // Needs no outputs
+  return true;
+}
+
+bool YOLOLossLayer::Connect ( const std::vector< CombinedTensor* >& inputs,
+                           const std::vector< CombinedTensor* >& outputs,
+                           const NetStatus* net ) {
+  UNREFERENCED_PARAMETER(net);
+  // Needs exactly three inputs to calculate the difference
+  if ( inputs.size() != 3 )
+    return false;
+
+  // Also, the two inputs have to have the same number of samples and elements!
+  // We ignore the shape for now...
+  CombinedTensor* first = inputs[0];
+  CombinedTensor* second = inputs[1];
+  CombinedTensor* third = inputs[2];
+  bool valid = first != nullptr && second != nullptr &&
+               outputs.size() == 0;
+
+  if ( valid ) {
+    first_ = first;
+    second_ = second;
+    third_ = third;
+  }
+
+  return valid;
+}
+
+void YOLOLossLayer::FeedForward() {
+  // We write the deltas at this point, because
+  // CalculateLossFunction() is called before BackPropagate().
+  // We don't precalculate the loss because it is not calculated for every
+  // batch.
+  //pragma omp parallel for default(shared)
+  for ( unsigned int sample = 0; sample < first_->data.samples(); sample++ ) {
+
+  }
+  first_->delta.Clear();
+}
+
+void YOLOLossLayer::BackPropagate() {
+  // The deltas are already written in to the input CombinedTensors, so
+  // there is nothing to do now.
+}
+
+datum YOLOLossLayer::CalculateLossFunction() {
+  long double error = 0;
+
+  for (unsigned int sample = 0; sample < first_->data.samples(); sample++) {
+				  error += 0;
+  }
+}
+
+
+}
