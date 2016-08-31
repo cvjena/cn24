de9e1b4327109184d3b5922182bfed124f74da9a
Clemens-Alexander Brust <ikosa.de@gmail.com>
2016-08-31 17:40:41 +0200

    [net] YOLOLossLayer: Feature complete

--- src/net/YOLOLossLayer.cpp
+++ src/net/YOLOLossLayer.cpp
@@ -96,15 +96,129 @@ bool YOLOLossLayer::Connect ( const std::vector< CombinedTensor* >& inputs,
 void YOLOLossLayer::FeedForward() {
   // We write the deltas at this point, because
   // CalculateLossFunction() is called before BackPropagate().
-  // We don't precalculate the loss because it is not calculated for every
-  // batch.
   //pragma omp parallel for default(shared)
-  for ( unsigned int sample = 0; sample < first_->data.samples(); sample++ ) {
-    LOGDEBUG << "Processing sample " << sample;
-    std::vector<BoundingBox>* sample_boxes = (std::vector<BoundingBox>*)second_->metadata[sample];
-    LOGDEBUG << "  " << sample_boxes->size() << " bounding boxes in ground truth";
+  first_->delta.Clear((datum)0);
+  current_loss_ = 0;
+
+  for (unsigned int sample = 0; sample < first_->data.samples(); sample++ ) {
+    // Clear output vector
+    std::vector<BoundingBox>* sample_boxes = (std::vector<BoundingBox>*)first_->metadata[sample];
+    std::vector<BoundingBox>* truth_boxes = (std::vector<BoundingBox>*)second_->metadata[sample];
+
+    // Prepare indices into the prediction array
+    unsigned int sample_index = sample * first_->data.maps();
+    unsigned int class_index = sample_index;
+    unsigned int confidence_index = sample_index + (classes_ * vertical_cells_ * horizontal_cells_);
+    unsigned int coords_index = confidence_index + vertical_cells_ * horizontal_cells_ * boxes_per_cell_;
+
+    // Loop over all cells
+    for (unsigned int vcell = 0; vcell < vertical_cells_; vcell++) {
+      for (unsigned int hcell = 0; hcell < horizontal_cells_; hcell++) {
+        unsigned int cell_id = vcell * horizontal_cells_ + hcell;
+        const datum box_xmin = ((datum)hcell) / (datum)horizontal_cells_;
+        const datum box_ymin = ((datum)vcell) / (datum)vertical_cells_;
+        const datum box_xmax = ((datum)(hcell + 1)) / (datum)horizontal_cells_;
+        const datum box_ymax = ((datum)(vcell+ 1)) / (datum)vertical_cells_;
+
+        // Find matching ground truth box
+        BoundingBox* truth_box;
+        bool found_box = false;
+        for(unsigned int t = 0; t < truth_boxes->size(); t++) {
+          BoundingBox* tbox = &((*truth_boxes)[t]);
+          if(tbox->x >= box_xmin && tbox->x < box_xmax && tbox->y >= box_ymin && tbox->y < box_ymax) {
+            truth_box = tbox;
+            found_box = true;
+            break;
+          }
+        };
+
+        datum best_iou = 0;
+        int responsible_box = -1;
+
+        // Loop over all possible boxes to find "responsible" box
+        if(found_box) {
+          for (unsigned int b = 0; b < boxes_per_cell_; b++) {
+            unsigned int box_coords_index = coords_index + 4 * (boxes_per_cell_ * cell_id + b);
+
+            // Calculate in-image coordinates
+            const datum x = box_xmin + (first_->data.data_ptr_const()[box_coords_index] / (datum) horizontal_cells_);
+            const datum y = box_ymin + (first_->data.data_ptr_const()[box_coords_index + 1] / (datum) vertical_cells_);
+            const datum w = first_->data.data_ptr_const()[box_coords_index + 2] *
+                            first_->data.data_ptr_const()[box_coords_index + 2];
+            const datum h = first_->data.data_ptr_const()[box_coords_index + 3] *
+                            first_->data.data_ptr_const()[box_coords_index + 3];
+
+            // Calculate actual IOU
+            BoundingBox box(x, y, w, h);
+            datum actual_iou = box.IntersectionOverUnion(truth_box);
+
+            if (actual_iou > best_iou) {
+              responsible_box = b;
+              best_iou = actual_iou;
+            }
+          }
+
+          // Loop over all classes to assign loss for each
+          for (unsigned int c = 0; c < classes_; c++) {
+            unsigned int cell_class_index = class_index + cell_id * classes_ + c;
+            const datum predicted_class_prob = first_->data.data_ptr_const()[cell_class_index];
+
+            const datum class_delta = predicted_class_prob - (truth_box->c == c ? (datum)1.0 : (datum)0.0);
+            first_->delta[cell_class_index] = class_delta;
+            current_loss_ += (class_delta * class_delta);
+          }
+        }
+
+        // Loop over all boxes to calculate loss
+        for (unsigned int b = 0; b < boxes_per_cell_; b++) {
+          // Box predicted iou is needed for both cases
+          unsigned int box_confidence_index = confidence_index + (boxes_per_cell_ * cell_id) + b;
+          const datum box_confidence = first_->data.data_ptr_const()[box_confidence_index];
+
+          if(((int)b) == responsible_box) {
+            // Box b is "responsible" for the detection
+            unsigned int box_coords_index = coords_index + 4 * (boxes_per_cell_ * cell_id + b);
+
+            // Calculate in-image coordinates
+            const datum x = box_xmin + (first_->data.data_ptr_const()[box_coords_index] / (datum) horizontal_cells_);
+            const datum y = box_ymin + (first_->data.data_ptr_const()[box_coords_index + 1] / (datum) vertical_cells_);
+            const datum w = first_->data.data_ptr_const()[box_coords_index + 2];
+            const datum h = first_->data.data_ptr_const()[box_coords_index + 3];
+
+            // Calculate actual IOU
+            BoundingBox box(x, y, w * w, h * h);
+            datum actual_iou = box.IntersectionOverUnion(truth_box);
+
+            // Loss: Box coordinates
+            const datum xcoord_delta = x - truth_box->x;
+            const datum ycoord_delta = y - truth_box->y;
+            first_->delta.data_ptr()[box_coords_index] = scale_coord_ * xcoord_delta;
+            first_->delta.data_ptr()[box_coords_index + 1] = scale_coord_ * ycoord_delta;
+            current_loss_ += (datum)(scale_coord_ * xcoord_delta * xcoord_delta) + (datum)(scale_coord_ * ycoord_delta * ycoord_delta);
+
+            // Loss: Box size
+            const datum w_delta = w - std::sqrt(truth_box->w);
+            const datum h_delta = h - std::sqrt(truth_box->h);
+            first_->delta.data_ptr()[box_coords_index + 2] = scale_coord_ * w_delta;
+            first_->delta.data_ptr()[box_coords_index + 3] = scale_coord_ * h_delta;
+            current_loss_ += (datum)(scale_coord_ * w_delta * w_delta) + (datum)(scale_coord_ * h_delta * h_delta);
+
+            // Loss: Predicted confidence
+            const datum conf_delta = box_confidence - actual_iou;
+            current_loss_ += (datum)(conf_delta * conf_delta);
+            first_->delta.data_ptr()[box_confidence_index] = conf_delta;
+          } else {
+            // Box b is not "responsible" for the ground truth
+
+            // Loss: Box confidence
+            first_->delta.data_ptr()[box_confidence_index] = scale_noobj_* box_confidence;
+            current_loss_ += (datum)(box_confidence * box_confidence);
+          }
+        }
+
+      }
+    }
   }
-  first_->delta.Clear();
 }
 
 void YOLOLossLayer::BackPropagate() {
@@ -113,12 +227,7 @@ void YOLOLossLayer::BackPropagate() {
 }
 
 datum YOLOLossLayer::CalculateLossFunction() {
-  long double error = 0;
-
-  for (unsigned int sample = 0; sample < first_->data.samples(); sample++) {
-				  error += 0;
-  }
-  return (datum)error;
+  return (datum)current_loss_;
 }
 
 
