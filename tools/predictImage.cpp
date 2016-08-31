d12687f42a237fbfce9a0223b2d3e122082e90eb
Clemens-Alexander Brust <ikosa.de@gmail.com>
2016-08-31 18:52:22 +0200

    [tools] predictImage: Added special case for detection

--- tools/predictImage.cpp
+++ tools/predictImage.cpp
@@ -19,14 +19,16 @@
 #include <cn24.h>
 
 int main (int argc, char* argv[]) {
-  if (argc < 6) {
-    LOGERROR << "USAGE: " << argv[0] << " <dataset config file> <net config file> <net parameter tensor> <input image file> <output image file>";
+  if (argc < 5) {
+    LOGERROR << "USAGE: " << argv[0] << " <dataset config file> <net config file> <net parameter tensor> <input image file> [<output image file>]";
     LOGEND;
     return -1;
   }
 
   // Capture command line arguments
-  std::string output_image_fname (argv[5]);
+  std::string output_image_fname;
+  if(argc > 5)
+    output_image_fname = argv[5];
   std::string input_image_fname (argv[4]);
   std::string param_tensor_fname (argv[3]);
   std::string net_config_fname (argv[2]);
@@ -49,111 +51,150 @@ int main (int argc, char* argv[]) {
   if(!dataset_config_file.good()) {
     FATAL("Cannot open dataset configuration file!");
   }
-  
+
   // Parse network configuration file
-  Conv::ConfigurableFactory* factory = new Conv::ConfigurableFactory(net_config_file, 238238, false);
+  Conv::JSON net_json = Conv::JSON::parse(net_config_file);
+  net_json["net"]["error_layer"] = "no";
+
+  Conv::JSONNetGraphFactory* factory = new Conv::JSONNetGraphFactory(net_json);
+
   // Parse dataset configuration file
-  Conv::TensorStreamDataset* dataset = Conv::TensorStreamDataset::CreateFromConfiguration(dataset_config_file, true);
+  Conv::JSON dataset_json = Conv::JSON::parse(dataset_config_file);
+  // Remove actual data to avoid loading times
+  dataset_json["data"] = Conv::JSON::array();
+
+  Conv::Dataset* dataset = Conv::JSONDatasetFactory::ConstructDataset(dataset_json);
   unsigned int CLASSES = dataset->GetClasses();
-  
+
   // Load image
   Conv::Tensor original_data_tensor(input_image_fname);
-  
-  // Rescale image
-  unsigned int width = original_data_tensor.width();
-  unsigned int height = original_data_tensor.height();
-  unsigned int original_width = original_data_tensor.width();
-  unsigned int original_height = original_data_tensor.height();
-  if(width & 1)
-    width++;
-  if(height & 1)
-    height++;
-  
-  if(width & 2)
-    width+=2;
-  if(height & 2)
-    height+=2;
-  
-  if(width & 4)
-    width+=4;
-  if(height & 4)
-    height+=4;
 
-  if(width & 8)
-    width+=8;
-  if(height & 8)
-    height+=8;
+  if(dataset->GetTask() == Conv::SEMANTIC_SEGMENTATION) {
 
-  if(width & 16)
-    width+=16;
-  if(height & 16)
-    height+=16;
-  
-  Conv::Tensor data_tensor(1, width, height, original_data_tensor.maps());
-  Conv::Tensor helper_tensor(1, width, height, 2);
-  data_tensor.Clear();
-  helper_tensor.Clear();
+    // Rescale image
+    unsigned int width = original_data_tensor.width();
+    unsigned int height = original_data_tensor.height();
+    unsigned int original_width = original_data_tensor.width();
+    unsigned int original_height = original_data_tensor.height();
 
-  // Copy sample because data_tensor may be slightly larger
-  Conv::Tensor::CopySample(original_data_tensor, 0, data_tensor, 0);
+    if (width & 1) width++; if (height & 1) height++;
+    if (width & 2) width += 2; if (height & 2) height += 2;
+    if (width & 4) width += 4; if (height & 4) height += 4;
+    if (width & 8) width += 8; if (height & 8) height += 8;
+    if (width & 16) width += 16; if (height & 16) height += 16;
 
-  // Initialize helper (spatial prior) tensor
+    Conv::Tensor data_tensor(1, width, height, original_data_tensor.maps());
+    Conv::Tensor helper_tensor(1, width, height, 2);
+    data_tensor.Clear();
+    helper_tensor.Clear();
 
-  // Write spatial prior data to helper tensor
-  for (unsigned int y = 0; y < original_height; y++) {
-    for (unsigned int x = 0; x < original_width; x++) {
-      *helper_tensor.data_ptr(x, y, 0, 0) = ((Conv::datum)x) / ((Conv::datum)original_width - 1);
-      *helper_tensor.data_ptr(x, y, 1, 0) = ((Conv::datum)y) / ((Conv::datum)original_height - 1);
+    // Copy sample because data_tensor may be slightly larger
+    Conv::Tensor::CopySample(original_data_tensor, 0, data_tensor, 0);
+
+    // Initialize helper (spatial prior) tensor
+
+    // Write spatial prior data to helper tensor
+    for (unsigned int y = 0; y < original_height; y++) {
+      for (unsigned int x = 0; x < original_width; x++) {
+        *helper_tensor.data_ptr(x, y, 0, 0) = ((Conv::datum) x) / ((Conv::datum) original_width - 1);
+        *helper_tensor.data_ptr(x, y, 1, 0) = ((Conv::datum) y) / ((Conv::datum) original_height - 1);
+      }
+      for (unsigned int x = original_width; x < width; x++) {
+        *helper_tensor.data_ptr(x, y, 0, 0) = 0;
+        *helper_tensor.data_ptr(x, y, 1, 0) = 0;
+      }
     }
-    for (unsigned int x = original_width; x < width; x++) {
-      *helper_tensor.data_ptr(x, y, 0, 0) = 0;
-      *helper_tensor.data_ptr(x, y, 1, 0) = 0;
+    for (unsigned int y = original_height; y < height; y++) {
+      for (unsigned int x = 0; x < height; x++) {
+        *helper_tensor.data_ptr(x, y, 0, 0) = 0;
+        *helper_tensor.data_ptr(x, y, 1, 0) = 0;
+      }
     }
+
+
+    // Assemble net
+    Conv::NetGraph graph;
+    Conv::InputLayer input_layer(data_tensor, helper_tensor);
+
+    Conv::NetGraphNode input_node(&input_layer);
+    input_node.is_input = true;
+
+    graph.AddNode(&input_node);
+    bool complete = factory->AddLayers(graph);
+    if (!complete) FATAL("Failed completeness check, inspect model!");
+
+    graph.Initialize();
+
+
+    // Load network parameters
+    graph.DeserializeParameters(param_tensor_file);
+
+    graph.SetIsTesting(true);
+    LOGINFO << "Classifying..." << std::flush;
+    graph.FeedForward();
+
+    Conv::Tensor *net_output_tensor = &graph.GetDefaultOutputNode()->output_buffers[0].combined_tensor->data; // &net.buffer(output_layer_id)->data;
+    Conv::Tensor image_output_tensor(1, net_output_tensor->width(), net_output_tensor->height(), 3);
+
+    LOGINFO << "Colorizing..." << std::flush;
+    dataset->Colorize(*net_output_tensor, image_output_tensor);
+
+    // Recrop image down
+    Conv::Tensor small(1, original_data_tensor.width(), original_data_tensor.height(), 3);
+    for (unsigned int m = 0; m < 3; m++)
+      for (unsigned int y = 0; y < small.height(); y++)
+        for (unsigned int x = 0; x < small.width(); x++)
+          *small.data_ptr(x, y, m, 0) = *image_output_tensor.data_ptr_const(x, y, m, 0);
+
+    small.WriteToFile(output_image_fname);
+  } else if(dataset->GetTask() == Conv::CLASSIFICATION || dataset->GetTask() == Conv::DETECTION) {
+    // Rescale image
+    unsigned int width = dataset->GetWidth();
+    unsigned int height = dataset->GetHeight();
+    unsigned int original_width = original_data_tensor.width();
+    unsigned int original_height = original_data_tensor.height();
+
+    Conv::Tensor data_tensor(1, width, height, original_data_tensor.maps());
+    data_tensor.Clear();
+    Conv::Tensor::CopySample(original_data_tensor, 0, data_tensor, 0, true);
+
+     // Assemble net
+    Conv::NetGraph graph;
+    Conv::InputLayer input_layer(data_tensor);
+
+    Conv::NetGraphNode input_node(&input_layer);
+    input_node.is_input = true;
+
+    graph.AddNode(&input_node);
+    bool complete = factory->AddLayers(graph);
+    if (!complete) FATAL("Failed completeness check, inspect model!");
+
+    graph.Initialize();
+
+    // Load network parameters
+    graph.DeserializeParameters(param_tensor_file);
+
+    graph.SetIsTesting(true);
+    LOGINFO << "Classifying..." << std::flush;
+    graph.FeedForward();
+
+    if(dataset->GetTask() == Conv::CLASSIFICATION) {
+      Conv::Tensor *net_output_tensor = &graph.GetDefaultOutputNode()->output_buffers[0].combined_tensor->data;
+      // TODO display scores
+    } else {
+      Conv::DatasetMetadataPointer* net_output = graph.GetDefaultOutputNode()->output_buffers[0].combined_tensor->metadata;
+      std::vector<Conv::BoundingBox>* output_boxes = (std::vector<Conv::BoundingBox>*)net_output[0];
+
+      LOGINFO << "Bounding boxes: " << output_boxes->size();
+      for(unsigned int b = 0; b < output_boxes->size(); b++) {
+        Conv::BoundingBox box = (*output_boxes)[b];
+        LOGINFO << "Box\t" << b << ": " << dataset->GetClassNames()[box.c] << " (" << box.score << ")";
+        LOGINFO << "  Center: (" << box.x * (Conv::datum)original_width << "," << box.y * (Conv::datum)original_height << ")";
+        LOGINFO << "  Size: (" << box.w * (Conv::datum)original_width << "x" << box.h * (Conv::datum)original_height << ")";
+      }
+    }
+
   }
-  for (unsigned int y = original_height; y < height; y++) {
-    for (unsigned int x = 0; x < height; x++) {
-      *helper_tensor.data_ptr(x, y, 0, 0) = 0;
-      *helper_tensor.data_ptr(x, y, 1, 0) = 0;
-    }
-  }
-  
-
-  // Assemble net
-	Conv::NetGraph graph;
-  Conv::InputLayer input_layer(data_tensor, helper_tensor);
-
-	Conv::NetGraphNode input_node(&input_layer);
-  input_node.is_input = true;
-
-	graph.AddNode(&input_node);
-	bool complete = factory->AddLayers(graph, Conv::NetGraphConnection(&input_node), CLASSES);
-	if (!complete)
-    FATAL("Failed completeness check, inspect model!");
-
-	graph.Initialize();
-
-
-  // Load network parameters
-  graph.DeserializeParameters(param_tensor_file);
-  
-  graph.SetIsTesting(true);
-  LOGINFO << "Classifying..." << std::flush;
-  graph.FeedForward();
-  
-	Conv::Tensor* net_output_tensor = &graph.GetDefaultOutputNode()->output_buffers[0].combined_tensor->data; // &net.buffer(output_layer_id)->data;
-  Conv::Tensor image_output_tensor(1, net_output_tensor->width(), net_output_tensor->height(), 3);
-  
-  LOGINFO << "Colorizing..." << std::flush;
-  dataset->Colorize(*net_output_tensor, image_output_tensor);
-  
-  // Recrop image down
-  Conv::Tensor small(1, original_data_tensor.width(), original_data_tensor.height(), 3);
-  for(unsigned int m = 0; m < 3; m++)
-    for(unsigned int y = 0; y < small.height(); y++)
-      for(unsigned int x = 0; x < small.width(); x++)
-        *small.data_ptr(x,y,m,0) = *image_output_tensor.data_ptr_const(x,y,m,0);
-
-  small.WriteToFile(output_image_fname);
 
   LOGINFO << "DONE!";
   LOGEND;
