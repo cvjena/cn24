05d7fb466e0828abac3636f7a83128bbbc716a0e
Clemens-Alexander Brust <ikosa.de@gmail.com>
2016-09-26 17:59:23 +0200

    [net] YOLODynamicOutputLayer: Feature complete

--- include/cn24/net/YOLODynamicOutputLayer.h
+++ include/cn24/net/YOLODynamicOutputLayer.h
@@ -8,6 +8,8 @@
 #ifndef CONV_YOLODYNAMICOUTPUTLAYER_H
 #define CONV_YOLODYNAMICOUTPUTLAYER_H
 
+#include <random>
+
 #include "SimpleLayer.h"
 #include "ClassManager.h"
 
@@ -26,7 +28,11 @@ public:
   void FeedForward();
   void BackPropagate();
   std::string GetLayerDescription() { return "YOLO Dynamic Output Layer"; }
+  void OnLayerConnect (const std::vector<Layer*> next_layer);
 
+  inline unsigned int Gain() {
+    return input_->data.maps();
+  }
 private:
   ClassManager* class_manager_;
   unsigned int horizontal_cells_ = 0;
@@ -37,6 +43,8 @@ private:
   CombinedTensor* box_biases_;
   CombinedTensor* class_weights_;
   CombinedTensor* class_biases_;
+
+  std::mt19937 rand_;
 };
 }
 
--- src/net/YOLODynamicOutputLayer.cpp
+++ src/net/YOLODynamicOutputLayer.cpp
@@ -5,12 +5,16 @@
  * For licensing information, see the LICENSE file included with this project.
  */
 
+#include <algorithm>
+
 #include "YOLODynamicOutputLayer.h"
 
 namespace Conv {
 
 YOLODynamicOutputLayer::YOLODynamicOutputLayer(JSON configuration, ClassManager *class_manager) :
   SimpleLayer(configuration), class_manager_(class_manager) {
+  unsigned int seed = 0;
+
   if(configuration.count("yolo_configuration") != 1) {
     FATAL("YOLO configuration missing!");
   }
@@ -31,6 +35,11 @@ YOLODynamicOutputLayer::YOLODynamicOutputLayer(JSON configuration, ClassManager 
   }
   boxes_per_cell_ = yolo_configuration["boxes_per_cell"];
 
+  if(configuration.count("seed") == 1 && configuration["seed"].is_number()) {
+    seed = configuration["seed"];
+  }
+
+  rand_.seed(seed);
 }
 
 void YOLODynamicOutputLayer::UpdateTensorSizes() {
@@ -67,6 +76,7 @@ bool YOLODynamicOutputLayer::Connect(const CombinedTensor *input, CombinedTensor
   class_biases_ = new CombinedTensor(horizontal_cells_ * vertical_cells_ * (class_manager_->GetMaxClassId() + 1), 1, 1, 1);
   class_biases_->is_dynamic = true;
 
+
   parameters_.push_back(box_weights_);
   parameters_.push_back(box_biases_);
   parameters_.push_back(class_weights_);
@@ -93,6 +103,12 @@ bool YOLODynamicOutputLayer::CreateOutputs(const std::vector<CombinedTensor *> &
     LOGERROR << "Null pointer input node!";
     return false;
   }
+
+  if(input->data.width() != 1 || input->data.height() != 1) {
+    LOGERROR << "Input needs to be 1x1";
+    return false;
+  }
+
   unsigned int output_maps = horizontal_cells_ * vertical_cells_ * (boxes_per_cell_ * 5 + (class_manager_->GetMaxClassId() + 1));
   CombinedTensor* output = new CombinedTensor (input->data.samples(), 1, 1, output_maps);
   output->is_dynamic = true;
@@ -128,6 +144,7 @@ void YOLODynamicOutputLayer::FeedForward() {
       for (unsigned int i = 0; i < input_maps; i++) {
         sum += input_ptr[i] * *(class_weights_->data.data_ptr_const(0, 0, i, m));
       }
+      output_ptr[m + class_offset] = sum;
     }
 
     // Box biases
@@ -137,12 +154,89 @@ void YOLODynamicOutputLayer::FeedForward() {
 
     // Class biases
     for (unsigned int m = 0; m < class_biases_->data.samples(); m++) {
-      output_ptr[m + class_offset] += box_biases_->data.data_ptr_const()[m];
+      output_ptr[m + class_offset] += class_biases_->data.data_ptr_const()[m];
     }
   }
 }
 
+void YOLODynamicOutputLayer::OnLayerConnect(const std::vector<Layer *> next_layers) {
+  unsigned int next_layer_gain = 0;
+  for (Layer* next_layer: next_layers)
+    next_layer_gain += next_layer->Gain();
+
+  unsigned int this_layer_gain = Gain();
+
+  const datum range = sqrt (6) / sqrt (next_layer_gain + this_layer_gain);
+  std::uniform_real_distribution<datum> dist_weights (-range , range);
+
+  for (std::size_t i = 0; i < box_weights_->data.elements(); i++) {
+    box_weights_->data[i] = dist_weights (rand_);
+  }
+  for (std::size_t i = 0; i < box_biases_->data.elements(); i++) {
+    box_biases_->data[i] = dist_weights (rand_);
+  }
+  for (std::size_t i = 0; i < class_weights_->data.elements(); i++) {
+    class_weights_->data[i] = dist_weights (rand_);
+  }
+  for (std::size_t i = 0; i < class_biases_->data.elements(); i++) {
+    class_biases_->data[i] = dist_weights (rand_);
+  }
+}
+
 void YOLODynamicOutputLayer::BackPropagate() {
+  const unsigned int class_offset = vertical_cells_ * horizontal_cells_ * boxes_per_cell_ * 5;
+  const unsigned int input_maps = input_->data.maps();
+  const unsigned int box_output_maps = vertical_cells_ * horizontal_cells_ * boxes_per_cell_ * 5;
+  const unsigned int class_output_maps = vertical_cells_ * horizontal_cells_ * (class_manager_->GetMaxClassId() + 1);
+
+  box_weights_->delta.Clear();
+  box_biases_->delta.Clear();
+  class_weights_->delta.Clear();
+  class_biases_->delta.Clear();
+
+  for (unsigned int sample = 0; sample < input_->data.samples(); sample++) {
+    const datum *input_data_ptr = input_->data.data_ptr_const(0, 0, 0, sample);
+    datum *input_delta_ptr = input_->delta.data_ptr(0, 0, 0, sample);
+    const datum *output_delta_ptr = output_->delta.data_ptr_const(0, 0, 0, sample);
+    const datum *output_data_ptr = output_->data.data_ptr_const(0, 0, 0, sample);
+
+    // Weight gradients
+    // Box weights
+    for (unsigned int m = 0; m < box_output_maps; m++) {
+      for (unsigned int i = 0; i < input_maps; i++) {
+        *box_weights_->delta.data_ptr(0, 0, i, m) += (output_delta_ptr[m] * input_data_ptr[i]);
+      }
+    }
+    // Class weights
+    for (unsigned int m = 0; m < class_output_maps; m++) {
+      for (unsigned int i = 0; i < input_maps; i++) {
+        *class_weights_->delta.data_ptr(0, 0, i, m) += (output_delta_ptr[m + class_offset] * input_data_ptr[i]);
+      }
+    }
+    // Box biases
+    for (unsigned int m = 0; m < box_output_maps; m++) {
+      box_biases_->delta.data_ptr()[m] += output_delta_ptr[m];
+    }
+    // Class biases
+    for (unsigned int m = 0; m < class_output_maps; m++) {
+      class_biases_->delta.data_ptr()[m] += output_delta_ptr[m + class_offset];
+    }
+
+    // Input gradients
+    for(unsigned int i = 0; i < input_maps; i++) {
+      datum delta = 0;
+      // Box weights
+      for (unsigned int m = 0; m < box_output_maps; m++) {
+        delta += output_delta_ptr[m] * *(box_weights_->data.data_ptr(0, 0, i, m));
+      }
+      // Class weights
+      for (unsigned int m = 0; m < class_output_maps; m++) {
+        delta += output_delta_ptr[m + class_offset] * *(class_weights_->data.data_ptr(0, 0, i, m));
+      }
+      input_delta_ptr[i] = delta;
+    }
+
+  }
 
 }
 }
