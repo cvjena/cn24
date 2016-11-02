cc451ee1be7001db1d44181a07302a411ec6af3a
Clemens-Alexander Brust <ikosa.de@gmail.com>
2016-11-02 22:41:38 +0100

    [tools] UIInfo: Added drawing of test image

--- tools/UIInfo.cpp
+++ tools/UIInfo.cpp
@@ -16,15 +16,43 @@
 
 int main() {
   Conv::System::Init(3);
-  Conv::NKContext context(1200, 800);
-  
-  LOGINFO << "Context at " << &context;
-  
-  while(1) {
-    context.ProcessEvents();
-    // ...
-    context.Draw();
+
+  uint8_t* test_image = new uint8_t[400*400*3];
+  for(int y = 0; y < 400; y++) {
+    for (int x = 0; x < 400; x++) {
+      test_image[3 * (y * 400 + x)] = x / 2;
+      test_image[3 * (y * 400 + x) + 1] = x / 2;
+      test_image[3 * (y * 400 + x) + 2] = x / 2;
+    }
   }
+  {
+    Conv::NKContext context(1200, 800);
+    bool running = true;
+    while (running) {
+      context.ProcessEvents();
+
+      if(nk_begin(context, "UI Information", nk_rect(0,0,300,200),
+      NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE | NK_WINDOW_CLOSABLE |
+      NK_WINDOW_TITLE)) {
+        nk_layout_row_dynamic(context, 30, 1);
+        if(nk_button_label(context, "Exit")) {
+          break;
+        }
+
+        nk_layout_row_dynamic(context, 400, 1);
+        struct nk_image demo_image = nk_image_ptr(test_image);
+        demo_image.w = 400;
+        demo_image.h = 400;
+        nk_image(context, demo_image);
+      }
+      nk_end(context);
+
+      if(nk_window_is_closed(context, "UI Information"))
+        break;
+      context.Draw();
+    }
+  }
+  delete[] test_image;
   LOGEND;
   
   return 0;
