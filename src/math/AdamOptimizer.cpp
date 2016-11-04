4964caa64ee611ea5e0173b4d3b8b19ba85f0ccb
Clemens-Alexander Brust <ikosa.de@gmail.com>
2016-11-04 23:05:57 +0100

    [math] AdamOptimizer: Added initialization heuristic (GD steps)

--- src/math/AdamOptimizer.cpp
+++ src/math/AdamOptimizer.cpp
@@ -18,22 +18,28 @@ AdamOptimizer::AdamOptimizer(JSON descriptor) : Optimizer(descriptor) {
 }
 
 void AdamOptimizer::Step(std::vector<Tensor> &buffers, CombinedTensor* parameters,
-                        unsigned int iteration) {
+                        unsigned int reported_iteration) {
+  UNREFERENCED_PARAMETER(reported_iteration);
+  // Do not use the reported iteration, it doesn't reset!
+  const float actual_iteration = iterations_since_reset_ + 1;
   Tensor& first_moment = buffers[0];
   Tensor& second_moment = buffers[1];
 
-  const datum current_step_size = step_size_ / sqrtf(iteration + 1);
+  const datum current_step_size = step_size_; /// sqrtf(actual_iteration);
 
   for(std::size_t p = 0; p < parameters->data.elements(); p++) {
     const datum g_t = parameters->delta(p);
     const datum m_t = beta1_ * first_moment(p) + ((Conv::datum)1.0 - beta1_) * g_t;
     const datum v_t = beta2_ * second_moment(p) + ((Conv::datum)1.0 - beta2_) * g_t * g_t;
 
-    const datum m_hat_t = m_t / ((Conv::datum)1.0 - (Conv::datum)pow(beta1_, iteration + 1.0));
-    const datum v_hat_t = v_t / ((Conv::datum)1.0 - (Conv::datum)pow(beta2_, iteration + 1.0));
+    const datum m_hat_t = m_t / ((Conv::datum)1.0 - (Conv::datum)pow(beta1_, actual_iteration));
+    const datum v_hat_t = v_t / ((Conv::datum)1.0 - (Conv::datum)pow(beta2_, actual_iteration));
 
-    parameters->data[p] = parameters->data(p) - current_step_size * (m_hat_t / (sqrtf(v_hat_t) + epsilon_));
-
+    if(actual_iteration > 2) {
+      parameters->data[p] = parameters->data(p) - current_step_size * (m_hat_t / (sqrtf(v_hat_t) + epsilon_));
+    } else {
+      parameters->data[p] = parameters->data(p) - current_step_size * g_t;
+    }
     // Save moments
     first_moment[p] = m_t;
     second_moment[p] = v_t;
