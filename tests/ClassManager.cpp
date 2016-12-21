e903d5e448bb557bfccf44b45ccec9d24f6377d1
Clemens-Alexander Brust <ikosa.de@gmail.com>
2016-12-21 23:47:44 +0100

    [tests] ClassManager: Fixed failing test

--- tests/ClassManager.cpp
+++ tests/ClassManager.cpp
@@ -17,7 +17,7 @@ int main() {
   Conv::ClassManager class_manager2;
 
   // Check if empty class manager is actually empty
-  Conv::AssertEqual<int>(UNKNOWN_CLASS, class_manager.GetMaxClassId(), "GetMaxClassId() on empty ClassManager");
+  Conv::AssertEqual<int>(0, class_manager.GetMaxClassId(), "GetMaxClassId() on empty ClassManager");
   Conv::AssertEqual<int>(0, class_manager.GetClassCount(), "GetClassCount() on empty ClassManager");
 
   // Register classes
