694a2f59f1ebe61661cd06a90f7d934e42ddf24f
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2017-04-05 17:08:31 +0200

    [dist] Fixed debian packaging

--- distribution/ubuntu-deb/build-package.sh
+++ distribution/ubuntu-deb/build-package.sh
@@ -4,7 +4,7 @@
 docker build -t cn24pkg .
 
 TMP_BU=$(pwd)
-cd ..
+cd ../..
 CN24_SRC=$(pwd)
 cd "$TMP_BU"
 docker run -it --name cn24pkgbuilder -v $CN24_SRC:/cn24-pkg-out/cn24-src cn24pkg bash /cn24-pkg-out/build-package.sh
--- distribution/ubuntu-deb/scripts/build-package.sh
+++ distribution/ubuntu-deb/scripts/build-package.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+
+# This script runs inside the container
 CN24_VERSION="3.0.0-SNAPSHOT"
 cd "${0%/*}"
 
-# Create "origin" tarball
-cd cn24-src
-tar -cz -f ../cn24_$CN24_VERSION.orig.tar.gz *
-cd ..
-
+echo "Making target directory"
 mkdir cn24-$CN24_VERSION
 cd cn24-$CN24_VERSION
-tar -x -f ../cn24_$CN24_VERSION.orig.tar.gz
-mv distribution/debian debian
 
-dpkg-buildpackage -uc -us -j12
+# Copy files from repo
+echo "Copying files..."
+shopt -s extglob
+cp -r ../cn24-src/!(distribution) ./
 
+echo "Moving debian control files..."
+cp -r ../cn24-src/distribution/ubuntu-deb/debian debian
+
+# Create "origin" tarball
+echo "Creating origin tarball..."
+tar -cz -f ../cn24_$CN24_VERSION.orig.tar.gz *
+
+echo "Building package..."
+debuild -us -uc -j12
 bash
